49029efcdc157a1e29dd0b79e7580fe19841cae0
tkrikoriam <tkrikoriam@achs.cl>
2021-03-03 06:09:42 -0300

    Add interface repository

---
+++ src/02/Start_Here/MyShop.Infrastructure/IRepository.cs
@@ -0,0 +1,11 @@
+namespace MyShop.Infrastructure.Repositories
+{
+    public interface IRepository<T>
+    {
+        T Add(T entity);
+        T Update(T entity);
+        T Get(Guid Id);
+        IEnumerable<T> All();
+        IEnumerable<T> Find(Expresssion<Func<T,bool>> predicate)
+    }
+}
